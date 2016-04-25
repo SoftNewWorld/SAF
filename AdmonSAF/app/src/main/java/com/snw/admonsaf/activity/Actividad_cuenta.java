f12580de73611bee6f5f4821586696c67da1b21a
Enriquez <roycee90@hotmail.com>
2016-04-25 17:02:48 -0500

    cree un paquete activity y pojo con sus variable

---
+++ AdmonSAF/app/src/main/java/com/snw/admonsaf/activity/Actividad_cuenta.java
@@ -0,0 +1,8 @@
+package com.snw.admonsaf.activity;
+
+/**
+ * Created by ENRIQUEZ on 25/04/2016.
+ */
+public class Actividad_cuenta {
+    private String numero;
+}
---
+++ AdmonSAF/app/src/main/java/com/snw/admonsaf/pojo/Cuenta.java
@@ -0,0 +1,9 @@
+package com.snw.admonsaf.pojo;
+
+/**
+ * Created by ENRIQUEZ on 25/04/2016.
+ */
+public class Cuenta {
+
+    private String consecutivo;
+}
