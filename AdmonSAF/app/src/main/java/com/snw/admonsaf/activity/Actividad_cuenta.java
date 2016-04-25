09ad68cd2db4de829f6552ed213ce2911a4a2ef4
Oscar Giraldo <oscardavid@jmspro.net>
2016-04-25 17:35:38 -0500

    Restaurar Master

--- AdmonSAF/app/src/main/java/com/snw/admonsaf/activity/Actividad_cuenta.java
+++
@@ -1,8 +0,0 @@
-package com.snw.admonsaf.activity;
-
-/**
- * Created by ENRIQUEZ on 25/04/2016.
- */
-public class Actividad_cuenta {
-    private String numero;
-}
--- AdmonSAF/app/src/main/java/com/snw/admonsaf/pojo/Cuenta.java
+++
@@ -1,9 +0,0 @@
-package com.snw.admonsaf.pojo;
-
-/**
- * Created by ENRIQUEZ on 25/04/2016.
- */
-public class Cuenta {
-
-    private String consecutivo;
-}
